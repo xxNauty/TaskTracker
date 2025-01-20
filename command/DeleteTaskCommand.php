65134b2ca9d2bcc0f48a0fdec6dbcd20c56fb7aa
Mateusz Wnuk <mateusz2003w@gmail.com>
2025-01-20 19:11:34 +0100

    files for commands logic

--- command/DeleteTaskCommand.php
+++ command/DeleteTaskCommand.php
@@ -1,37 +1,6 @@
 <?php
 
-namespace command;
-
-use entity\Task;
-
 class DeleteTaskCommand
 {
-    private string $data {
-        get {
-            return $this->data;
-        }
-        set {
-            $this->data = $value;
-        }
-    }
 
-    private Task $task {
-        get {
-            return $this->task;
-        }
-        set {
-            $this->task = $value;
-        }
-    }
-
-    public function __construct(string $data, Task $task)
-    {
-        $this->data = $data;
-        $this->task = $task;
-    }
-
-
-    public function execute(): string{
-
-    }
 }
--- main.php
+++ main.php
@@ -1,24 +1,11 @@
 <?php
 
-include_once 'database/DatabaseConnectionManager.php';
-include_once 'entity/Task.php';
-include_once 'entity/TaskCreator.php';
+require_once 'database/DatabaseConnectionManager.php';
+require_once 'entity/Task.php';
+require_once 'entity/TaskCreator.php';
+require_once 'command/CreateTaskCommand.php';
 
 $connection = new DatabaseConnectionManager();
-$connection->openConnection();
 
-$taskCreator = new TaskCreator($connection);
-$taskCreator->createTask('Test1', 'high');
-$taskCreator->createTask('Test2', 'low');
-
-$connection->saveData();
-$connection->closeConnection();
-
-$connection2 = new DatabaseConnectionManager();
-$connection2->openConnection();
-
-$tasks = $connection2->findAllTasks();
-//var_dump($tasks);
-echo count($tasks)."\n";
-
-$connection2->closeConnection();
+$command = new CreateTaskCommand($connection);
+$command->action();
